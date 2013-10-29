44cf57c2bf6b141958572c30a1cdbcd7d3b1fa5f
Sharvil <sharv@umich.edu>
2013-10-29 13:29:56 -0400

    changed config to match oracle config options with drivers

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,11 +1,14 @@
 <?php
 return array(
     'db' => array(
-        'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=caen;host=localhost',
-        'driver_options' => array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
+        // 'driver'         => 'Pdo',
+        // 'dsn'            => 'mysql:dbname=caen;host=localhost',
+        // 'driver_options' => array(
+        //     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        // ),
+        'driver'            => 'OCI8',
+        'connection_string' => 'jdbc:oracle:thin:@oratest.engin.umich.edu:1521:engin',
+        'character_set'     => 'AL32UTF8',
     ),
     'service_manager' => array(
         'factories' => array(
